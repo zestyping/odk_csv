33b030f295149f35258a71303aed58a507d62549
Ka-Ping Yee <zestyping@gmail.com>
2017-09-06 12:49:52 +0300

    Report conversion problems to the user.  Add more flexible CSV delimiter detection.

--- odk_csv.py
+++ odk_csv.py
@@ -1,10 +1,9 @@
 from PyQt4.QtGui import QAction, QFileDialog, QIcon, QMessageBox
-from qgis.core import QgsMessageLog
 
 import csv
 import os
 
-GEO_FIELDS = ['drain_line']
+GEOTRACE_FIELDS = ['drain_line']
 
 
 class OdkCsvConverter(object):
@@ -33,40 +32,83 @@ class OdkCsvConverter(object):
             dir, name = os.path.split(path)
             self.last_dir = dir
             name, ext = os.path.splitext(path)
-            new_path = name + '-qgis' + ext
+            new_path = name + '.qgis' + ext
 
-            convert_csv(path, new_path)
-            QMessageBox.information(
-                self.main, 'Wrote CSV', 'Wrote CSV output to: %s' % new_path)
+            conversions, errors = convert_csv(path, new_path)
+            message = 'Wrote CSV output to: ' + new_path
+            if errors:
+                row, field = errors[0]
+                line = row + 2  # row 0 is on line 2 of the file
+                QMessageBox.warning(self.main, 'Warning', '''
+Some problems were found and ignored.
+
+Geotraces converted: %d
+Problems found: %d
+First problem: %r field on line %d
+
+(An example of a valid geotrace field is "-6.796 39.269 27 3.2;-6.795 39.262 29 3.4".  Each point should consist of four numbers followed by a semicolon; the numbers are latitude, longitude, altitude, and accuracy.)
+
+Fields with problems were left unchanged.  Wrote CSV output to: %s
+''' % (len(conversions), len(errors), field, line, new_path))
+            else:
+                QMessageBox.information(self.main, 'Success', '''
+All geotraces were converted successfully.
+
+Geotraces converted: %d
+
+Wrote CSV output to: %s
+''' % (len(conversions), new_path))
 
 
 def convert_csv(input_path, output_path):
-    # KoboToolbox exports "CSV" files with semicolons as delimiters instead
-    # of commas; we detect the delimiter by examining the header row.
-    with open(input_path) as infile:
-        comma_fields = csv.reader(infile, delimiter=',').next()
-    with open(input_path) as infile:
-        semicolon_fields = csv.reader(infile, delimiter=';').next()
-    delimiter = (len(semicolon_fields) > len(comma_fields)) and ';' or ','
-
+    """Given the path to an input CSV file, converts the contents of all the
+    fields named in GEOTRACE_FIELDS from JavaRosa format to WKT format, and
+    writes a new CSV file at the given output path."""
+    delimiter = detect_csv_delimiter(input_path)
+    conversions, errors = [], []
     with open(input_path) as infile:
         with open(output_path, 'w') as outfile:
             reader = csv.DictReader(infile, delimiter=delimiter)
             writer = csv.DictWriter(outfile, reader.fieldnames)
             writer.writeheader()
-            for row in reader:
-                for name in GEO_FIELDS:
+            for r, row in enumerate(reader):
+                for name in GEOTRACE_FIELDS:
                     if name in row:
-                        row[name] = convert_to_wkt(row[name])
+                        try:
+                            row[name] = convert_to_wkt(row[name])
+                        except (TypeError, ValueError):
+                            errors.append((r, name))
+                        else:
+                            conversions.append((r, name))
                 writer.writerow(row)
+    return conversions, errors
 
 
 def convert_to_wkt(geotrace):
+    """Converts a Geotrace string in JavaRosa format to a WKT LINESTRING.
+
+    The JavaRosa format consists of points separated by semicolons, where each
+    point is given as four numbers (latitude, longitude, altitude, accuracy)
+    separated by spaces."""
     wkt_points = []
     for point in geotrace.split(';'):
         if point.strip():
             lat, lon, alt, accuracy = map(float, point.split())
-            wkt_points.append('%.6f %.6f %.0f %.1f' % (lon, lat, alt, accuracy))
-    if wkt_points:
-        return 'LINESTRING ZM(%s)' % (', '.join(wkt_points))
-    return ''
+            wkt_points.append('%.6f %.6f %d %.1f' % (lon, lat, alt, accuracy))
+    return wkt_points and 'LINESTRING ZM (%s)' % (', '.join(wkt_points)) or ''
+
+
+def detect_csv_delimiter(path):
+    """Detects which field deliimter to use for parsing a CSV-like file."""
+    # We assume that the correct delimiter is the one that causes the greatest
+    # number of unique field names to be parsed from the header row.
+    candidates = [(0, ',')]  # default to comma
+    for delimiter in [',', '\t', ';']:
+        with open(path) as infile:
+            try:
+                field_names = csv.reader(infile, delimiter=delimiter).next()
+            except:
+                continue
+            candidates.append((len(set(field_names)), delimiter))
+    most_fields, best_delimiter = max(candidates)
+    return best_delimiter
